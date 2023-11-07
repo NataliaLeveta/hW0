9ea87c09bf454a43bbf484e2b3b7e549bb8c54b6
NataliaLeveta <61879991+NataliaLeveta@users.noreply.github.com>
2023-11-08 01:58:50 +0200

    Update collatz.c collatz

--- collatz/src/collatz.c
+++ collatz/src/collatz.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[]){ 
-    int num1 = atoi(argv[1]);  //
-    int num2 = atoi(argv[2]); 
-	int sum, sumax = 0;
+int main (int argc, char *argv[]){ // takes arguments from the command line
+    int num1 = atoi(argv[1]);  // num1 : lower limit of number range
+    int num2 = atoi(argv[2]); // num2 : high limit of number range
+	int c, cmax = 0; // c : counter , cmax : max counter
 	long long n;
-    if(num1 >= 1 && num1 <= 100000000 && num2 >= 1 && num2 <= 100000000 && num1 < num2 && argc ==3){
-	    while (num1 <= num2){
-		    n = num1;
-		    sum = 0;
-		    while (n>1){
-			    if (n%2 == 0){
-				    n = n/2;
-				    sum++;
+    if(num1 >= 1 && num1 <= 100000000 && num2 >= 1 && num2 <= 100000000 && num1 < num2 && argc == 3){ 
+	    while (num1 <= num2){ // the first given number has to be smaller or equal than the second 
+		    n = num1; // n takes the first number we gave 
+		    c = 0; // initialize the counter
+		    while (n > 1){
+			    if (n%2 == 0){ // if the number is even
+				    n = n/2; 
+				    c++; // then the count increases by one unit
 			    }
-			    else {
+			    else { // if the number is odd
 				    n = 3 * n + 1;
-				    sum++;
+				    c++; // then the count increases by one unit
 			    }
 		    }
-		    sum++;
-		    if (sum > sumax)
-			    sumax = sum;
-	    num1++;
+		    c++; // include the number 1 to the final counter 
+		    if (c > cmax) // find the biggest counter 
+			    cmax = c; 
+	    num1++; // go to the next number
 	    }
-	    printf ("%d\n", sumax);
+	    printf ("%d\n", cmax);
     }
     else 
         printf("0\n");
 }
-

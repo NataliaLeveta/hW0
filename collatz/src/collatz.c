ee3d8b7879f786f6373cb7cb4af645833203a7a0
NataliaLeveta <61879991+NataliaLeveta@users.noreply.github.com>
2023-11-07 03:17:22 +0200

    Update collatz.c collatz

--- collatz/src/collatz.c
+++ collatz/src/collatz.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]){ 
+    int num1 = atoi(argv[1]);  //
+    int num2 = atoi(argv[2]); 
+	int sum, sumax = 0;
+	long long n;
+    if(num1 >= 1 && num1 <= 100000000 && num2 >= 1 && num2 <= 100000000 && num1 < num2 && argc ==3){
+	    while (num1 <= num2){
+		    n = num1;
+		    sum = 0;
+		    while (n>1){
+			    if (n%2 == 0){
+				    n = n/2;
+				    sum++;
+			    }
+			    else {
+				    n = 3 * n + 1;
+				    sum++;
+			    }
+		    }
+		    sum++;
+		    if (sum > sumax)
+			    sumax = sum;
+	    num1++;
+	    }
+	    printf ("%d\n", sumax);
+    }
+    else 
+        printf("0\n");
+}
 
